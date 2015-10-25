fe270f64674830ae6f02248c33c7e99cf777dfc5
Marcelo Martins <marcelo.a.martins@gmail.com>
2015-10-25 10:24:03 -0200

    Bug fixes

--- source/memoryLeakFixed.c
+++ source/memoryLeakFixed.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void wasteMemory(int i) {
-	printf("\nMem Addr Allocated: %d", i);
-   	double *ptr = (double*) malloc(1000*sizeof(double));
-
-	printf("\nMem Addr Unallocated: %d\n\n", i);
-   	free(ptr);   //Solution
-}
-
-int main() {
-	int i = 0;
+void main() {
 	while(1 == 1) {
-		i++;
-		wasteMemory(i);
+	   	double *ptr = (double*) malloc(1000*sizeof(double));
+	   	printf("\nMem Addr Allocated:   0x%x", ptr);
+	   	free(ptr);
+	   	printf("\nMem Addr Unallocated: 0x%x\n\n", ptr);
 	}
 }
